8144898dd027ebe6ba619cdcc1cdc91593c27875
oliwiat22 <oliwiatrojniak@gmail.com>
2024-01-28 11:45:18 +0100

    poprawa

--- src/API/productApi.js
+++ src/API/productApi.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 export const SERVER_URL = 'http://localhost:8080/api/shopping-list';
 
 export const addProduct = (name, price) => {
-    const url = `${SERVER_URL}/add/`;
+    const url = `${SERVER_URL}/add`;
     const newProduct= {
         name: name, 
         price: price,
